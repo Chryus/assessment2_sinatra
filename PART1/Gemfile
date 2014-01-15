2dcf1a4c833f7dad48c35dc067299c9e0d2b8499
Chris <chris.haack@me.com>
2014-01-15 14:19:50 -0500

    answer questions

---
+++ PART1/Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+ 
+gem "sinatra"
+gem "sqlite3"
+gem "rake"
+gem "haml"
+ 
+group :development do
+  gem "shotgun"
+  gem "tux"
+end
---
+++ PART1/app.rb
@@ -0,0 +1,54 @@
+require 'bundler'
+Bundler.require
+
+module Rabbits
+  class App < Sinatra::Application
+
+    set :database, "sqlite3:///database.db"
+
+    Dir.glob('./lib/*.rb') do |model|
+      require model
+    end
+
+    get '/' do 
+      @cats = SpaceCat.all
+      haml :all
+    end
+
+    get 'new' do
+      @cat = SpaceCat.new
+      haml :new
+    end
+
+    get 'show' do
+      @cat = SpaceCat.all(params[:id])
+      haml :show
+    end
+
+    post 'create' do
+      @cat = SpaceCat.new(params[:id])
+      haml :create
+    end
+
+    get 'edit' do
+      @cat = SpaceCat.find(params[:id])
+      haml :edit
+    end
+
+    post 'update' do
+      @cat = SpaceCat.find(params[:id])
+    end
+
+    get 'delete' do
+      @cat = SpaceCat.delete(params[:id])
+      haml 
+    end
+
+    helpers do
+      def partial(file_name)
+        erb file_name, layout => false
+      end
+    end
+
+  end
+end
